73629b864136b1f66fd69b1b88d62b8bdfe5360c
Brody <brodyrhartman@gmail.com>
2026-05-03 22:52:32 -0400

    Added final implementation for 6 test cases.

--- tests/src/Gui/Dialogs/DlgUndoRedo.cpp
+++ tests/src/Gui/Dialogs/DlgUndoRedo.cpp
@@ -28,6 +28,7 @@ class testDlgUndoRedo final: public QObject
 	public:
 		testDlgUndoRedo()
 		{
+			//This creates the application which inits the MDIView, the Document
 			tests::initApplication();
 		}
         
@@ -43,38 +44,56 @@ private Q_SLOTS:
 	void testUndoAddAllActions() // NOLINT
 	{
 		//Arrange
-		MDIView* mdi =  getMainWindow()->activeWindow();
-		Gui::UndoDialog dialog = new UndoDialog(mockMdi);
+		//Get pointer to active view and get active document
+		MDIView* mdi = getMainWindow()->activeWindow();
+		Document test = mdi->getGuiDocument();
+		UndoDialog undo = new UndoDialog(mdi);
+		//adds command to transaction list
+		test::openCommand("Mock");
 		//Act 
+		//call onFetchInfo to add the actions to the MDI undoActions list
+		undo::onFetchInfo();
+		//get the list from the mdi
+		QStringList vecUndos = mdi->undoActions();
 		//Assert
-		Q_ASSERT();
+		//There should only be one item in the list 
+		Q_ASSERT(vecUndos.size() == 1);
 	}
-
 	void testUndoNoActiveWindow() // NOLINT
 	{
 		//Arrange
-		//Creates a new UndoDialog using the nullpointer mdi we created.
-		//This constructor also calls the UndoDialog::OnFetchInfo which executes the code that checks the mdi
-		MDIView* mdi =  getMainWindow()->activeWindow();
-		Gui::UndoDialog dialog =  new UndoDialog(mdi);
+		//Get pointer to active view
+		MDIView* mdi = getMainWindow()->activeWindow();
+		UndoDialog undo = new UndoDialog(mdi);
 		//Act 
-        dialog.onFetchInfo();
+		//close the active view
+		mdi::deleteSelf();
 		//Assert
-		Q_ASSERT();
+		//QT Test will catch an exception if a crash occurs for having no active view to undo
+		QVERIFY_THROWS_EXCEPTION(std::exception, undo::onFetchInfo());
 	}
 	void testUndoSelectedActsExcludesA() // NOLINT
 	{
 		//Arrange
+		MDIView* mdi = getMainWindow()->activeWindow();
+		UndoDialog undo = new UndoDialog(mdi);
+		//adds commands to transaction list
+		std::vector<std::string> undoStringBefore = test::getUndoVector();
+		test::openCommand("Mock 1");
+		test::openCommand("Mock 2");
+		test::openCommand("Mock 3");
+		test::openCommand("Mock 4");
 		//Act 
+		//Undoing all the mock commands
+		undo::onSelected();
+		undo::onSelected();
+		undo::onSelected();
+		undo::onSelected();
+		//checks the available undos after clearing the commands
+		std::vector<std::string> undoStringAfter = test::getUndoVector();
 		//Assert
-		//Q_ASSERT();
-	}
-	void testUndoSelectedActsIncludesA() // NOLINT
-	{
-		//Arrange
-		//Act 
-		//Assert
-		//Q_ASSERT();
+		//The availble undo actions before and after the test should be the same
+		Q_ASSERT(undoStringAfter == undoStringBefore);
 	}
 	void testSelectUndoandRedoEmpty() // NOLINT
 	{
@@ -121,20 +140,62 @@ private Q_SLOTS:
 	void testSavingThenUndo() // NOLINT
 	{
 		//Arrange
+		//Get pointer to active view and get active document
+		MDIView* mdi = getMainWindow()->activeWindow();
+		UndoDialog undo = new UndoDialog(mdi);
+		Document test = mdi->getGuiDocument();
 		//Act 
+		//Saving but not closing the file
+		test::save();
 		//Assert
-		//Q_ASSERT();
+		//Test will catch an exception if the onFetchInfo() causes an exception for undoing after saving
+		QVERIFY_THROWS_EXCEPTION(std::exception, undo::onFetchInfo());
 	}
 	void testCloseClearsHistory() // NOLINT
 	{
 		//Arrange
+		//Get pointer to active view and get active document
+		MDIView* mdi = getMainWindow()->activeWindow();
+		UndoDialog undo = new UndoDialog(mdi);
+		Document test = mdi->getGuiDocument();
+		//adds command to transaction list
+		test::openCommand("Mock");
 		//Act 
+		//checks to see it can undo last command
+		std::vector<std::string> undoStringBefore = test::getUndoVector();
+		//saves then closes and reopens the document object
+		test::save();
+		test::close();
+		test::restore();
+		//checks the undo list expecting empty transaction list
+		std::vector<std::string> undoStringAfter = test::getUndoVector();
 		//Assert
-		//Q_ASSERT();
+		//Tests if closing will maintain the same undo list
+		Q_ASSERT(undoStringBefore != undoStringAfter);
 	} 
-
-	
-    
+	void testNewActionClearsRedo() // NOLINT
+	{
+		//Arrange
+		//Get pointer to active view and get active document
+		MDIView* mdi = getMainWindow()->activeWindow();
+		RedoDialog redo = new RedoDialog(mdi);
+		UndoDialog undo = new UndoDialog(mdi);
+		Document test = mdi->getGuiDocument();
+		//adds command to transaction list
+		test::openCommand("Mock");
+		//undoes the action
+		undo::onSelected();
+		//Act 
+		//checks the redo
+		std::vector<std::string> redoStringBefore = test::getRedoVector();
+		//overwrites redo list by adding new command to transaction list
+		test::openCommand("Mock 2");
+		//checks for empty redo list
+		std::vector<std::string> redoStringAfter = test::getRedoVector();
+		//Assert
+		//Tests that new actions clear the redo queue
+		Q_ASSERT(redoStringBefore != redoStringAfter);
+	} 
 };
 
 QTEST_MAIN(testDlgUndoRedo)
